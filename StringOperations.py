f3b3daf973a841453f8e5279d2b395975e54c2bb
BadBryan44567 <71113321+BadBryan44567@users.noreply.github.com>
2022-10-29 00:42:59 +0530

    Update

--- StringOperations.py
+++ StringOperations.py
@@ -5,6 +5,7 @@ def check_sentence_ending(sentence: str):
 	return sentence.endswith('.')
 
 def clean_up_spacing(sentence: str):
+	
 	return sentence.strip()
 
 def replace_word_choice(sentence: str, old_word: str, new_word: str):
